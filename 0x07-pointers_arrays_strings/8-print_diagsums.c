3d2e57b9a883d4eebe193dce81d800286cc95f47
pigeondan <dannyboym3@gmail.com>
2024-01-08 11:19:39 -0800

    task 101

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -30,24 +30,3 @@ void print_diagsums(int *a, int size)
 	}
 	printf("%d, %d\n", count1, count2);
 }
-
-
-Task 100
-
-#include "main.h"
-
-/**
-  * set_string - main function
-  *
-  * @s: Function parameter
-  *
-  * @to: Function parameter
-  *
-  * Return: Always 0.
-  */
-void set_string(char **s, char *to)
-
-{
-	*s = to;
-
-}
