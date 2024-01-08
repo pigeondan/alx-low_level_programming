705cfee5f8f32dee407c1d04527a70ade80e229f
pigeondan <dannyboym3@gmail.com>
2024-01-08 11:04:11 -0800

    task 7

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,53 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  * print_diagsums - main function
+  *
+  * @size: function parameter
+  *
+  * @a: function parameter
+  *
+  * Return: Always 0.
+  */
+
+void print_diagsums(int *a, int size)
+{
+	int count1;
+	int count2;
+	int x;
+
+	count1 = 0;
+	count2 = 0;
+
+	for (x = 0; x < size; x++)
+	{
+	count1 = count1 + a[x * size + x];
+	}
+	for (x = size - 1; x >= 0; x--)
+	{
+	count2 += a[x * size + (size - x - 1)];
+	}
+	printf("%d, %d\n", count1, count2);
+}
+
+
+Task 100
+
+#include "main.h"
+
+/**
+  * set_string - main function
+  *
+  * @s: Function parameter
+  *
+  * @to: Function parameter
+  *
+  * Return: Always 0.
+  */
+void set_string(char **s, char *to)
+
+{
+	*s = to;
+
+}
